8a11d9a22e00c84ab79f3dff6e80b7224f9c37a7
John Ruggles <ruggles@catuslabs.info>
2021-06-18 00:21:37 +0100

    Introduced Page Object Pattern

---
+++ cypress/support/pageObjects/WhatHappened.js
@@ -0,0 +1,20 @@
+export default class WhatHappened {  
+    getBackground() {
+        return cy.get('div[class="page__background"]', {timeout: 30000});
+    }
+    getAMurder() { 
+        return cy.get('div').contains('A murder has been committed in an alleyway outside a pub.', {timeout: 30000});
+    }
+    getJudgeThis() {
+        return cy.get('div').contains('Press \'Judge This\' once you have watched it.', {timeout: 30000});
+    }
+    getJudgeThisButton() {
+        return cy.get('span').contains('JUDGE THIS', {timeout: 30000});
+    }
+    getVideoIframe() {
+        return cy.get('iframe[id="video1"]', {timeout: 600000});
+    }
+    getWatchVideo() { 
+        return cy.get('div').contains('Watch the video to find out exactly what happened.', {timeout: 30000});
+    }
+}
